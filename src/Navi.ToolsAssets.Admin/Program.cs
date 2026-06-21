8c8100fcc4fe293cb942d1c2a26b91f54821eab8
Yeison David Quinto <ydeath2@gmail.com>
2026-06-20 19:14:44 -0500

    Aplica fuente Poppins en Admin Blazor

--- src/Navi.ToolsAssets.Admin/Program.cs
+++ src/Navi.ToolsAssets.Admin/Program.cs
@@ -2,17 +2,19 @@ using Navi.ToolsAssets.Admin.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("NaviApi", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["NaviApi:BaseUrl"] ?? "http://localhost:5218");
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
